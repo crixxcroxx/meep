3565d01d853384d6d87f75c3c9278d922c58212b
Cris Montajes <cristajesawa@gmail.com>
2023-09-18 08:41:06 +0800

    Create Users, Profiles & Follows

--- meep/social/models.py
+++ meep/social/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+# Create User Profile Model
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    follows = models.ManyToManyField(
+        "self",
+        related_name="followed_by",
+        symmetrical=False,  # if you follow someone they're NOT required to follow you back
+        blank=True,  # you're not required to follow anyone
+    )
+
+    def __str__(self):
+        return self.user.username
