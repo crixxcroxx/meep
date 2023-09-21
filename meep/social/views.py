6333c7df8eb4814028331cfb03f4d524a1de7c1c
Cris Montajes <cristajesawa@gmail.com>
2023-09-21 09:53:42 +0800

    Add login and logout

--- meep/social/views.py
+++ meep/social/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from .models import Profile, Meep
 from .forms import MeepForm
+from django.contrib.auth import authenticate, login, logout
 
 
 # Create your views here.
@@ -56,3 +57,26 @@ def profile(request, pk):
     else:
         messages.success(request, ("You must be logged in to view this page"))
         return redirect("home")
+
+
+def login_user(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, ("Successfully logged in"))
+            return redirect("home")
+        else:
+            messages.success(request, ("There is a problem logging in"))
+            return redirect("login")
+
+    else:
+        return render(request, "login.html", {})
+
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, ("You have been logged out"))
+    return redirect("home")
